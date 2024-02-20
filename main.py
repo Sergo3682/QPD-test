0f8303e694f47c5a694ecbb968a63bc174a05566
Sergo3682 <97442751+Sergo3682@users.noreply.github.com>
2024-02-20 16:21:27 +0300

    firts try to solve the problem

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+number = 1
+max_nums = ''
+max_sum = 0
+
+while(int(number) != 0):
+    cur_sum = 0
+    number = input('Enter a decimel number:\n')
+    for digit in number:
+        cur_sum += int(digit)
+    if cur_sum == max_sum:
+        max_nums += number + ' '
+    elif cur_sum > max_sum:
+        max_sum = cur_sum
+        max_nums = number + ' '
+
+print(f'The maximum sum of digits is {max_sum}\n\
+List of numbers with this maximum sum of digits: {max_nums}')
+
