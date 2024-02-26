d09d986a7cb827de9383aa7dc4eabdf1dd628c0b
Sergo3682 <97442751+Sergo3682@users.noreply.github.com>
2024-02-26 17:43:55 +0300

    add some changes to make script more stable

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 #!/usr/bin/python3
 
+import re
+
+validInputPattern = "^-?[0-9]+$"
+
+def getNumber():
+    isValid = False
+    while not isValid:
+        number = input('Enter an integer number:\n')
+        if re.match(validInputPattern, number):
+            isValid = True
+        else:
+            print(f'Invalid input. Please try again.')
+    return number
+
 number = 1
-max_nums = ''
-max_sum = 0
+maxNums = ''
+maxSum = 0
 
 while(int(number) != 0):
-    cur_sum = 0
-    number = input('Enter a decimel number:\n')
+    curSum = 0
+    number = getNumber()
     for digit in number:
          if digit != '-':
-            cur_sum += int(digit)
-    if cur_sum == max_sum:
-        max_nums += number + ' '
-    elif cur_sum > max_sum:
-        max_sum = cur_sum
-        max_nums = number + ' '
+            curSum += int(digit)
+    if curSum == maxSum:
+        maxNums += number + ' '
+    elif curSum > maxSum:
+        maxSum = curSum
+        maxNums = number + ' '
 
-print(f'The maximum sum of digits is {max_sum}\n\
-List of numbers with this maximum sum of digits: {max_nums}')
+print(f'The maximum sum of digits is {maxSum}\n\
+List of numbers with this maximum sum of digits: {maxNums}')
 
